0c75bfaf6bc159b524c5fcc4e318bf4ce7491506
nicolegooding <ngooding@uw.edu>
2021-02-25 21:42:19 -0800

    Updating my bar graph
    it doesn't work still waiting for TA input

--- src/NicoleViz.js
+++ src/NicoleViz.js
@@ -2,42 +2,57 @@
 import by_race from '../static/by_race.csv'
 
 //set up height, width, margin
+var container;
 var height = 700;
 var width = 950;
-var margin = ({top: 10, right: 10, bottom: 20, left: 35});
+var margin = ({top: 10, right: 10, bottom: 35, left: 35});
+var dataArray = [];
+
+d3.csv(by_race).then(function(data) {
+    data.forEach(function(d){
+      dataArray.push(d);
+    })
+    makeStaticViz();
+  });
+
+console.log(dataArray);
+
+console.log(dataArray.map(a => a.uof_percent));
 
 //scale functions
 var x = d3.scaleBand()
-    .domain(by_race.map(d => d.race))
+    .domain(dataArray.map(d => d.race))
     .range([margin.left, width - margin.right]);
 var y = d3.scaleLinear() 
     .domain([-1, 1])
     .range([height - margin.bottom, margin.top]);
 
 
-makeStaticViz();
+
 function makeStaticViz() {
     container = d3.select('#staticBar')
         .append('svg')
-        .att("id", "basic-chart")
+        .attr("id", "basic-chart")
         .attr('width', width)
         .attr('height', height);
     
-    bars = container.selectAll('rect')
-        .data(uof_for_me)
+    var bars = container
+        .append('svg')
+        .selectAll('rect')
+        .data(dataArray)
         .join('rect')
-        .attr('x', d => x_new(d.race))  
-        .attr('y', d => (y_new_per(d.uof_percent)))
-        .attr('width', x_new.bandwidth())
-        .attr('height', d => height / 2 - y_new_per(d.uof_percent))
+        .attr('x', d => x(d.race))  
+        .attr('y', d => (y(d.uof_percent)))
+        .attr('width', x.bandwidth())
+        .attr('height', d => height / 2 - y(d.uof_percent))
         .style('fill', 'steelblue')
         .style('stroke', 'white')
     ;
 
   // position and populate the x-axis
-    xAxis = container.append('g')
+    var xAxis = container.append('g')
         .attr('transform', `translate(0, ${height - margin.bottom})`)
-        .call(d3.axisBottom(x_new))
+        .call(d3.axisBottom(x))
         .append('text')
         .attr('text-anchor', 'end')
         .attr('fill', 'white')
@@ -48,7 +63,7 @@ function makeStaticViz() {
         .text('total UOF Percents');     
 
   // position and populate the y-axis
-    yAxis = container.append('g')
+    var yAxis = container.append('g')
         .attr('transform', `translate(${margin.left}, 0)`)
-        .call(d3.axisLeft(y_new_per));
+        .call(d3.axisLeft(y));
 }
